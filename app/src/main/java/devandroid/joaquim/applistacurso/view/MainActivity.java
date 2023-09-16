208a8dcb95b6b326bcf6cdb20ed8e436269b77dd
JoaquimLP <joaquimlp2013@gmail.com>
2023-09-16 16:32:15 -0300

    update layout spinner

--- app/src/main/java/devandroid/joaquim/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/joaquim/applistacurso/view/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_spinner);
 
         preferences = getSharedPreferences(NOME_PREFERENCES, 0);
         listaVip = preferences.edit();
